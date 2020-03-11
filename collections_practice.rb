2f1fafb93c54a500a07d0ca9c1a807df17ba5310
Ruari Day <ruariday@gmail.com>
2020-03-11 14:03:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,5 +5,5 @@ def sort_array_desc(array)
   array.sort { |a, b| b <=> a }
 end
 def sort_array_char_count(array)
-  array.sort { |a, b| a <=> b }
+  array.sort { |a, b| a.length <=> b.length }
 end 
