8e32dc8e4376db6f78f3614c681c1aee8ac74040
Ruari Day <ruariday@gmail.com>
2020-03-11 14:18:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
-
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b }
+end
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end 
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+end
+def find_a(array)
+  array.select do |word|
+    word[0] == "a"
+  end 
+end 
+def sum_array(array)
+  total = 0 
+  array.each do |num|
+    total += num
+  end 
+  total
+end
+def add_s(array)
+  array.map do |word|
+    if array[1] == word 
+      word
+    else 
+      word + "s"
+    end 
+  end
+end
